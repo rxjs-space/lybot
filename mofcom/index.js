596fa2b5979be026412de9bef2a9a6102b4ee783
lynx0421 <lynx0421@qq.com>
2017-12-26 10:42:09 +0800

    minro fix

--- mofcom/index.js
+++ mofcom/index.js
@@ -9,6 +9,7 @@ const Jimp = require("jimp");
 const Rx = require('rxjs/Rx');
 
 const isProduction = process.env.NODE_ENV === 'production';
+console.log('process.env.NODE_ENV is', process.env.NODE_ENV);
 
 const vehicleTypeXPathHash = require('./constants').vehicleTypeXPathHash;
 const lastInputElementXPathHash = require('./constants').lastInputElementXPathHash;
